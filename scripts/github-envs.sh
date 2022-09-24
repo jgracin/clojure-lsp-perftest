5acf3d89b0cf59691866943543c2580edc748bcd
Josip Gracin <josip.gracin@gmail.com>
2022-09-24 20:48:15 +0200

    w

--- scripts/github-envs.sh
+++ scripts/github-envs.sh
@@ -8,9 +8,11 @@ LATEST_COMMIT_TODAY=`git rev-list -n 1 --first-parent --since="$TODAY 00:00" $BR
 if [ -z "$LATEST_COMMIT_TODAY" ]; then
     # no commits today, nevertheless we must run tests on the last
     # two commits (see README.md for the explanation)
+    echo A=1
     echo BASELINE_COMMIT=`git rev-list -n 2 --first-parent $BRANCH | tail -n1`
     echo MEASURED_COMMIT=`git rev-list -n 2 --first-parent $BRANCH | head -n1`
 else
+    echo B=1
     echo BASELINE_COMMIT=$PREVIOUS_COMMIT
     echo MEASURED_COMMIT=$LATEST_COMMIT_TODAY
 fi
